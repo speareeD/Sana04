98ca59d9317078d4384fd03b2a61810a3451962e
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-12-06 23:05:58 +0200

    Added fields for the class and declared a constructor

--- Program/MyDate.cs
+++ Program/MyDate.cs
@@ -2,6 +2,19 @@
 {
     internal class MyDate
     {
+        protected int Day;
+        protected int Month;
+        protected int Year;
+        protected int Hours;
+        protected int Minutes;
 
+        public MyDate(int day, int month, int year, int hours, int minutes)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+            Hours = hours;
+            Minutes = minutes;
+        }
     }
 }
