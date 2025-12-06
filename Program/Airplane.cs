bc6f70a032ad2c33f84f2b3e128275b9b36324b0
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-12-06 23:12:18 +0200

    Added fields and created construtor

--- Program/Airplane.cs
+++ Program/Airplane.cs
@@ -2,6 +2,17 @@
 {
     internal class Airplane
     {
+        protected string StartCity;
+        protected string FinishCity;
+        protected MyDate StartDate;
+        protected MyDate FinishDate;
 
+        public Airplane(string startCity, string finishCity, MyDate startDate, MyDate finishDate)
+        {
+            StartCity = startCity;
+            FinishCity = finishCity;
+            StartDate = new MyDate(startDate);
+            FinishDate = new MyDate(finishDate);
+        }
     }
 }
