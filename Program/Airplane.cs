156238f8ec08912d7ec84a5c17e200c332f12716
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-12-07 13:09:42 +0200

    Added three constructors: copy, overload of main and default

--- Program/Airplane.cs
+++ Program/Airplane.cs
@@ -15,6 +15,30 @@
             FinishDate = new MyDate(finishDate);
         }
 
+        public Airplane(string startCity, string finishCity, MyDate startDate, MyDate finishDate, string airplaneName)
+        {
+            StartCity = startCity;
+            FinishCity = finishCity;
+            StartDate = new MyDate(startDate);
+            FinishDate = new MyDate(finishDate);
+        }
+
+        public Airplane(Airplane obj)
+        {
+            StartCity = obj.StartCity;
+            FinishCity = obj.FinishCity;
+            StartDate = new MyDate(obj.StartDate);
+            FinishDate = new MyDate(obj.FinishDate);
+        }
+
+        public Airplane()
+        {
+            StartCity = "Kyiv";
+            FinishCity = "Budapest";
+            StartDate = new MyDate();
+            FinishDate = new MyDate(1, 6, 15);
+        }
+
         public void GetTotalTime()
         {
             int totalHours = ((FinishDate.GetHours() - StartDate.GetHours()) * 60 +
