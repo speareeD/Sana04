4315b6da4e22be5e0822bc3c3eb4325b909144a5
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-12-08 16:44:05 +0200

    Added copy, default and overload of constructor

--- Program/Currency.cs
+++ Program/Currency.cs
@@ -11,6 +11,24 @@
             ExRate = exRate;
         }
 
+        public Currency(string name)
+        {
+            Name = name;
+            ExRate = 1.0;
+        }
+
+        public Currency(Currency obj)
+        {
+            Name = obj.Name;
+            ExRate = obj.ExRate;
+        }
+
+        public Currency()
+        {
+            Name = "USD";
+            ExRate = 1.5;
+        }
+
         public string GetName() => Name;
         public double GetExRate() => ExRate;
 
