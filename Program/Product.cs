04f5b1db107f4026914367a5b0be55db2bb5db72
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-12-08 16:48:44 +0200

    Added copy, default and overload of constructor

--- Program/Product.cs
+++ Program/Product.cs
@@ -18,5 +18,33 @@
             Producer = producer;
             Weight = weight;
         }
+
+        public Product(string name, double price, Currency cost)
+        {
+            Name = name;
+            Price = price;
+            Cost = new Currency(cost);
+            Quantity = 1;
+            Producer = "Unknown";
+            Weight = 0.0;
+        }
+        public Product(Product obj)
+        {
+            Name = obj.Name;
+            Price = obj.Price;
+            Cost = new Currency(obj.Cost);
+            Quantity = obj.Quantity;
+            Producer = obj.Producer;
+            Weight = obj.Weight;
+        }
+        public Product()
+        {
+            Name = "Generic Product";
+            Price = 10.0;
+            Cost = new Currency();
+            Quantity = 1;
+            Producer = "Default Producer";
+            Weight = 1.0;
+        }
     }
 }
