31eab69ed8f3db77f3f27258d82879ff8aa1269f
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-12-08 16:40:47 +0200

    Added fields, getters, setters and constructor for currency class

--- Program/Currency.cs
+++ Program/Currency.cs
@@ -2,6 +2,25 @@
 {
     internal class Currency
     {
+        protected string Name;
+        protected double ExRate;
 
+        public Currency(string name, double exRate)
+        {
+            Name = name;
+            ExRate = exRate;
+        }
+
+        public string GetName() => Name;
+        public double GetExRate() => ExRate;
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+        public void SetExRate(double exRate)
+        {
+            ExRate = exRate;
+        }
     }
 }
