eff058cb0873b4691ade16318287a3aa787031e2
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-12-08 16:48:00 +0200

    Added fields and a constructor to Product class

--- Program/Product.cs
+++ Program/Product.cs
@@ -2,6 +2,21 @@
 {
     internal class Product
     {
+        protected string Name;
+        protected double Price;
+        protected Currency Cost;
+        protected int Quantity;
+        protected string Producer;
+        protected double Weight;
 
+        public Product(string name, double price, Currency cost, int quantity, string producer, double weight)
+        {
+            Name = name;
+            Price = price;
+            Cost = new Currency(cost);
+            Quantity = quantity;
+            Producer = producer;
+            Weight = weight;
+        }
     }
 }
