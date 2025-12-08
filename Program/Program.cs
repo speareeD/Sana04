f6c71723b4769776bc1777710cbed57d79ff815b
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-12-08 17:03:13 +0200

    Added Product class instance, called availaible methods and made the output prettier for both airplane and product instances in the console

--- Program/Program.cs
+++ Program/Program.cs
@@ -8,9 +8,27 @@
                 "New York",
                 "Los Angeles",
                 new MyDate(1, 1, 2024, 10, 30),
-                new MyDate(2, 1, 2024, 13, 15)
+                new MyDate(1, 1, 2024, 13, 15)
             );
             airplane.GetTotalTime();
+            bool arrivingToday = airplane.IsArrivingToday();
+            Console.WriteLine($"You will arrive today: {arrivingToday}\n");
+
+            Product product = new Product(
+                "Laptop",
+                999.99,
+                new Currency("USD", 42.16),
+                25,
+                "Acer",
+                2.5
+
+            );
+            double priceInUAH = product.GetPriceInUAH();
+            Console.WriteLine($"Price of {product.GetName()} {product.GetProducer()} is {priceInUAH} UAH");
+            double totalPriceUAH = product.GetTotalPriceInUAH();
+            Console.WriteLine($"Total price of {product.GetName()} {product.GetProducer()} available in warehouse is {totalPriceUAH}");
+            double totalWeight = product.GetTotalWeight();
+            Console.WriteLine($"Total weight of all {product.GetName()} {product.GetProducer()} is {totalWeight}");
         }
     }
 }
